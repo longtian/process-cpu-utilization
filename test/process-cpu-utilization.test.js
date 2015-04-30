fa897b005b289762216b3b72e86f792b27ea4349
wangyan <wyvernnot@gmail.com>
2015-04-30 17:46:48 +0800

    unit test

--- test/process-cpu-utilization.test.js
+++ test/process-cpu-utilization.test.js
@@ -5,29 +5,33 @@
 var pcu = require('../');
 var assert = require('assert');
 
+while (process.uptime() < 1);
 describe('Process-cpu-utilization', function () {
-    it('should work', function (done) {
-        pcu.get(['%cpu', '%mem'], function (err, data) {
-            assert.equal(Object.keys(data).length, 2);
-            assert.ok(data['%cpu']);
-            done();
-        });
+    it('should get ready', function (done) {
+        pcu.ready(done);
     });
-    it('should work for virtual memory size', function (done) {
-        pcu.get(['vsize'], function (err, data) {
-            done();
+    describe('when called', function () {
+        it('should work', function (done) {
+            pcu.get(['%cpu', '%mem'], function (err, data) {
+                assert.equal(Object.keys(data).length, 2);
+                assert.ok(data['%cpu']);
+                done();
+            });
         });
-    });
-    it('should work for virtual memory size', function (done) {
-        pcu.get(['vsize'], function (err, data) {
-            done();
+        it('should work for virtual memory size', function (done) {
+            pcu.get(['vsize'], function (err, data) {
+                done();
+            });
         });
-    });
-
-    it('should work for CPU', function (done) {
-        pcu.cpu(function (err, percentage) {
-            console.log(arguments);
-            done();
+        it('should work for virtual memory size', function (done) {
+            pcu.get(['vsize'], function (err, data) {
+                done();
+            });
         });
-    });
+        it('should work for CPU', function (done) {
+            pcu.cpu(function (err, percentage) {
+                done();
+            });
+        });
+    })
 });
